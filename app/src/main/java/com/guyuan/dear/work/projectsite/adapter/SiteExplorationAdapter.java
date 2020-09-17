a6e830e68dbd7c38cbfe93b09599cdc88cfbbc9f
jannonx <791273579@qq.com>
2020-09-17 17:35:35 +0800

    feat 增加adapter模板文件

---
+++ app/src/main/java/com/guyuan/dear/work/projectsite/adapter/SiteExplorationAdapter.java
@@ -0,0 +1,33 @@
+package com.guyuan.dear.work.projectsite.adapter;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import com.guyuan.dear.base.adapter.BaseRecyclerAdapter;
+import com.guyuan.dear.base.bean.SimpleTabBean;
+
+import java.util.List;
+
+import tl.com.easy_recycleview_library.BaseRecyclerViewHolder;
+
+/**
+ * @description:
+ * @author: Jannonx
+ * @since: 2020/9/17 11:42
+ * @company: 固远（深圳）信息技术有限公司
+ */
+public class SiteExplorationAdapter extends BaseRecyclerAdapter<SimpleTabBean> {
+    public SiteExplorationAdapter(Context context, @NonNull List<SimpleTabBean> listData, int layoutID) {
+        super(context, listData, layoutID);
+    }
+
+    @Override
+    protected void bindDataToView(BaseRecyclerViewHolder holder, SimpleTabBean item,
+                                  int position) {
+//        holder.setText(R.id.tv_produce_type_name, item.getProductName());
+
+
+
+    }
+}
