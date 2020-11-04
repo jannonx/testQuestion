6b22775b15495276e9075b0dbe0f1430b15c9f2a
hughsheng <805358359@qq.com>
2020-11-04 20:25:27 +0800

    增加图标工具类

---
+++ app/src/main/java/com/guyuan/dear/work/assess/api/WorkAssessApiService.java
@@ -0,0 +1,14 @@
+package com.guyuan.dear.work.assess.api;
+
+import com.guyuan.dear.base.api.BaseApiService;
+
+/**
+ * @author : tl
+ * @description :
+ * @since: 2020/11/4 19:22
+ * @company : 固远（深圳）信息技术有限公司
+ **/
+
+public interface WorkAssessApiService extends BaseApiService {
+
+}
